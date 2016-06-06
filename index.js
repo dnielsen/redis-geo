b74da38c626527974868783a2698369776e13681
Dave Nielsen <dnielsen@gmail.com>
2016-06-05 22:47:23 -0700

    removed client.quit();

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ client.on('error', function (err) {
 });
 
 //add a new location to the set
+// For more detials, visit http://www.redis.io/commands/geoadd
 app.get('/geoadd', function (req, res) {
   // geoadd?set=locations&longitude=-122&latitude=37&city=San%20Francisco2
   myset = req.query.set; // "locations"
@@ -21,6 +22,7 @@ app.get('/geoadd', function (req, res) {
 });
 
 //output the geohash
+// For more detials, visit http://www.redis.io/commands/geohash
 app.get('/geohash', function (req, res) {
   // geohash?set=locations&city=San%20Francisco
   myset = req.query.set; // "locations"
@@ -31,6 +33,7 @@ app.get('/geohash', function (req, res) {
 });
 
 //output the geopos
+// For more detials, visit http://www.redis.io/commands/geopos
 app.get('/geopos', function (req, res) {
   // geopos?set=locations&city=San%20Francisco
   myset = req.query.set; // "locations"
@@ -41,6 +44,7 @@ app.get('/geopos', function (req, res) {
 });
 
 // output the distance between to cities
+// For more detials, visit http://www.redis.io/commands/geodist
 app.get('/geodist', function (req, res) {
   //$ geodist?set=locations&city1=San%20Francisco&city2=Rome&distance=mi
   myset = req.query.set; // "locations"
@@ -52,15 +56,24 @@ app.get('/geodist', function (req, res) {
 	})
 });
 
-// output the list of locations within the radius of a latitude and longitude
+// Given a longitude and latitude, output a list of locations within a radius 
+// For more detials, visit http://www.redis.io/commands/georadius
 app.get('/georadius', function (req, res) {
 	// client.georadius('locations', 'Romeo', '-122', '32', 'mi', function(err, results) {
 	client.georadius('locations', '-122', '32', '100', 'mi', function(err, results) {
-	res.send("Locations: " + results);
+  //$ geodist?set=locations&city1=San%20Francisco&city2=Rome&units=mi
+  myset = req.query.set; // "locations"
+  longitude = req.query.longitude; // "San%20Francisco"
+  latitude = req.query.latitude; // "Rome"
+  myunits = req.query.units; // "mi"
+  client.georadius(myset, longitude, latitude, mydistance, function(err, results) {
+	res.send("Distance between " + mycity1 + " and " + mycity2 + " is: " + results + " " + myunits); 
+	})
 	});
 });
 
 // output the list of locations within the radius of a member of the set
+// For more detials, visit http://www.redis.io/commands/georadiusbymember
 app.get('/georadiusbymember', function (req, res) {
 	// client.georadiusbymember('locations', 'Romeo', '100', 'mi', function(err, results) {
 	client.georadiusbymember('locations', 'Romeo', '100', 'mi', function(err, results) {
@@ -78,9 +91,12 @@ app.get('/zrange', function (req, res) {
 
 // remove one of the cities from the list of locations
 app.get('/zrem', function (req, res) {
+	// zrem?set=locations&city=San%20Francisco
 	myset = req.query.set; // "locations"
-	mycity = req.query.name; // "San Francisco"
+	mycity = req.query.name; // "San%20Francisco"
   	client.zrem(myset, mycity);
+	res.send("Removed " + mycity + " from " + myset); 
+	})
 });
 
 var server = app.listen(3000, function () {
